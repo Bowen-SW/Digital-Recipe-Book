21490ef481616705c4970d9108b28c65c0dc30d4
Bowen-SW <swb0006@uah.edu>
2018-02-13 11:10:49 -0600

    Restructured a little

---
+++ digital-recipe-book/src/digitalrecipebook/DigitalRecipeBook.java
@@ -0,0 +1,15 @@
+package digitalrecipebook;
+
+/**
+ * @author Spencer B
+ */
+public class DigitalRecipeBook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ digital-recipe-book/src/main/DigitalRecipeBook.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * @author Spencer B
+ */
+public class DigitalRecipeBook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
